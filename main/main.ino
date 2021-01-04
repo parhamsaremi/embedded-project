8c4cce4633e12874424ccc0cc3c97e727f89cf3f
savaw <savaw97@gmail.com>
2021-01-04 15:53:36 +0330

    Add some files

--- main/main.ino
+++ main/main.ino
@@ -1,6 +1,60 @@
+/**************************************************************************
+ This is an example for our Monochrome OLEDs based on SSD1306 drivers
+
+ Pick one up today in the adafruit shop!
+ ------> http://www.adafruit.com/category/63_98
+
+ This example is for a 128x64 pixel display using I2C to communicate
+ 3 pins are required to interface (two I2C and one reset).
+
+ Adafruit invests time and resources providing this open
+ source code, please support Adafruit and open-source
+ hardware by purchasing products from Adafruit!
+
+ Written by Limor Fried/Ladyada for Adafruit Industries,
+ with contributions from the open source community.
+ BSD license, check license.txt for more information
+ All text above, and the splash screen below must be
+ included in any redistribution.
+ **************************************************************************/
+
+#include <SPI.h>
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
 #include <Keypad.h>
-#include <Wire.h> 
-#include <LiquidCrystal.h>
+
+
+#define SCREEN_WIDTH 128 // OLED display width, in pixels
+#define SCREEN_HEIGHT 64 // OLED display height, in pixels
+
+// Declaration for an SSD1306 display connected to I2C (SDA, SCL pins)
+#define OLED_RESET     A3 // Reset pin # (or -1 if sharing Arduino reset pin)
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
+
+#define NUMFLAKES     10 // Number of snowflakes in the animation example
+
+#define LOGO_HEIGHT   16
+#define LOGO_WIDTH    16
+static const unsigned char PROGMEM logo_bmp[] =
+{ B00000000, B11000000,
+  B00000001, B11000000,
+  B00000001, B11000000,
+  B00000011, B11100000,
+  B11110011, B11100000,
+  B11111110, B11111000,
+  B01111110, B11111111,
+  B00110011, B10011111,
+  B00011111, B11111100,
+  B00001101, B01110000,
+  B00011011, B10100000,
+  B00111111, B11100000,
+  B00111111, B11110000,
+  B01111100, B11110000,
+  B01110000, B01110000,
+  B00000000, B00110000 };
+
+
 
 const byte ROWS = 4, COLS = 4;
 
@@ -17,64 +71,114 @@ byte colPins[COLS] = {5, 4, 3, 2};
 Keypad customKeypad = Keypad(makeKeymap(hexaKeys), rowPins, colPins, ROWS, COLS);
 
 
-LiquidCrystal lcd(A0,A1,A2,A3,A4,A5);
+#define WAIT 0
+#define GET_QR 1
+#define SHOW_QR 2
+#define OPEN_DOOR 3
 
 
-enum states {
-  WAIT,
-  GET_QR,
-  SHOW_QR,
-  OPEN_DOOR,
-};
+unsigned char state;
 
-states state = WAIT;
+void print_char(char c);
+void print_str(char* c);
 
+void setup() {
+  Serial.begin(9600);
 
-void setup(){
-  lcd.begin(16,2); 
+  // SSD1306_SWITCHCAPVCC = generate display voltage from 3.3V internally
+  if(!display.begin(SSD1306_SWITCHCAPVCC, 0x3D)) { // Address 0x3D for 128x64
+    Serial.println(F("SSD1306 allocation failed"));
+    for(;;); // Don't proceed, loop forever
+  }
+  display.display();
+  delay(100);
+
+  state = WAIT;
+
 }
 
-void loop(){
-  // char customKey = customKeypad.getKey();
-  // if (customKey){
-  //   lcd.clear();
-  //   lcd.setCursor(0, 0); 
-  //   lcd.print(customKey);
-  // }
-
+void loop() {
   switch (state)
   {
   case WAIT:
+  {
+     print_str("enter 'A' to get QR code");
+
     char customKey = customKeypad.getKey();
-    if(customKey == 'A')
+
+    if(customKey)
     {
-      state = GET_QR;
+
+      print_char(customKey);
+      if(customKey == 'A')
+      {
+            state = GET_QR;
+      }
     }
-    break;
   
+    break;
+  }
   case GET_QR:
-    lcd.clear();
-    lcd.setCursor(0, 0); 
-    lcd.print("Please wait..."); 
+  {
+     print_str("get");
+
     //TODO: ask server for a qr code and wait for server response 
     //then go to show_qr state
-    
+    print_str("please wait...");
+    delay(1500);
+    state= SHOW_QR;
     break;
-
+  }
   case SHOW_QR:
+  {
+     print_str("SOME_QR");
+     delay(1500);
+
     //TODO: show the qr
     //then wait for server to send response
     //wait for xx seconds 
     //if response was go to state open door
     //else go to idle (and print message on lcd)
-    break;
-
-  case OPEN_DOOR:
-    //TODO: open the door, wait for xx seconds, close the door
-    state = WAIT;
-    break;
-
-  default:
+    state = OPEN_DOOR;
     break;
   }
+  case OPEN_DOOR:
+  {
+      print_str("opening...");
+      delay(1500);
+
+      //TODO: open the door, wait for xx seconds, close the door
+      state = WAIT;
+    break;
+  }
+  default:
+  {
+  print_str("???");
+  delay(500);
+    break;
+  }
+  }
 }
+
+void print_char(char c)
+{
+  display.clearDisplay();
+  display.setTextSize(1);      // Normal 1:1 pixel scale
+  display.setTextColor(SSD1306_WHITE); // Draw white text
+  display.setCursor(0, 0);     // Start at top-left corner
+  display.cp437(true);         // Use full 256 char 'Code Page 437' font
+  display.write(c);
+  display.display();
+ }
+
+ void print_str(char* c)
+ {
+
+  display.clearDisplay();
+  display.setTextSize(1);      // Normal 1:1 pixel scale
+  display.setTextColor(SSD1306_WHITE); // Draw white text
+  display.setCursor(0, 0);     // Start at top-left corner
+  display.cp437(true);         // Use full 256 char 'Code Page 437' font
+  display.println(c);
+  display.display();
+  }
