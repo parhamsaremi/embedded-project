8b8bf642519ea3334d9f3ce34e4ddf4f9a21e144
Savaw <savaw97@gmail.com>
2021-01-06 17:41:15 +0330

    Show a default QR code

--- main/main.ino
+++ main/main.ino
@@ -1,29 +1,10 @@
-/**************************************************************************
- This is an example for our Monochrome OLEDs based on SSD1306 drivers
-
- Pick one up today in the adafruit shop!
- ------> http://www.adafruit.com/category/63_98
-
- This example is for a 128x64 pixel display using I2C to communicate
- 3 pins are required to interface (two I2C and one reset).
-
- Adafruit invests time and resources providing this open
- source code, please support Adafruit and open-source
- hardware by purchasing products from Adafruit!
-
- Written by Limor Fried/Ladyada for Adafruit Industries,
- with contributions from the open source community.
- BSD license, check license.txt for more information
- All text above, and the splash screen below must be
- included in any redistribution.
- **************************************************************************/
-
 #include <SPI.h>
 #include <Wire.h>
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
 #include <Keypad.h>
 
+#include "default_qr.h"
 
 #define SCREEN_WIDTH 128 // OLED display width, in pixels
 #define SCREEN_HEIGHT 64 // OLED display height, in pixels
@@ -36,24 +17,6 @@ Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
 
 #define LOGO_HEIGHT   16
 #define LOGO_WIDTH    16
-static const unsigned char PROGMEM logo_bmp[] =
-{ B00000000, B11000000,
-  B00000001, B11000000,
-  B00000001, B11000000,
-  B00000011, B11100000,
-  B11110011, B11100000,
-  B11111110, B11111000,
-  B01111110, B11111111,
-  B00110011, B10011111,
-  B00011111, B11111100,
-  B00001101, B01110000,
-  B00011011, B10100000,
-  B00111111, B11100000,
-  B00111111, B11110000,
-  B01111100, B11110000,
-  B01110000, B01110000,
-  B00000000, B00110000 };
-
 
 
 const byte ROWS = 4, COLS = 4;
@@ -120,8 +83,6 @@ void loop() {
   }
   case GET_QR:
   {
-     print_str("get");
-
     //TODO: ask server for a qr code and wait for server response 
     //then go to show_qr state
     print_str("please wait...");
@@ -131,8 +92,10 @@ void loop() {
   }
   case SHOW_QR:
   {
-     print_str("SOME_QR");
-     delay(1500);
+     display.clearDisplay(); //for Clearing the display
+     display.drawBitmap(0, 0, default_QR, 128, 64, WHITE); // display.drawBitmap(x position, y position, bitmap data, bitmap width, bitmap height, color)
+     display.display();
+     delay(2000);
 
     //TODO: show the qr
     //then wait for server to send response
